4abbe08fb95d4596fbd9d0087081bced3538d622
seenu4selenium <seenu4selenium@gmail.com>
2026-05-19 21:00:08 -0600

    Screesnhot code updated

--- src/test/java/com/h2ojewellery_testscenarios/TS_02_Login_Logout.java
+++ src/test/java/com/h2ojewellery_testscenarios/TS_02_Login_Logout.java
@@ -1,13 +1,19 @@
 package com.h2ojewellery_testscenarios;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.time.Duration;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.io.FileHandler;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterClass;
@@ -44,7 +50,7 @@ public class TS_02_Login_Logout {
 	}
 
 	@Test(description = "Verify H2o login page will display  all fields")
-	public void tc_009() {
+	public void tc_009() throws Exception {
 
 		driver.get("https://seenu4selenium.github.io/devices-management-Webpage/#");
 
@@ -60,9 +66,22 @@ public class TS_02_Login_Logout {
 		} else {
 			System.out.println("UserName editbox is NOT displayed on login page#############");
 		}
-
+		takeCurrentPageScreenshot("tc_009");
 		System.out.println("tc_009 test case PASSED!");
+		
+//		click on Login button without any data
+		driver.findElement(By.xpath("//*[@id='loginModal']/div/div/button")).click();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		takeCurrentPageScreenshot("tc_009_ErrorMessages");
 
+	}
+
+	@Test
+	public void tc_010() throws Exception {
+		//Singup link clicked
+		driver.findElement(By.xpath("//*[@id='loginModal']/div/div/div[5]/a[1]")).click();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		takeCurrentPageScreenshot("tc_010");
 	}
 
 	public void validateWebElement(By locator) {
@@ -72,6 +91,11 @@ public class TS_02_Login_Logout {
 		} else {
 			System.out.println(locator + " is NOT displayed on signup page*****");
 		}
+	}
+
+	public void takeCurrentPageScreenshot(String name) throws Exception {
+		File myscreen = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+		FileHandler.copy(myscreen, new File(".\\screenshots\\" + name + ".png"));
 	}
 
 }
